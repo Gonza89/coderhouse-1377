861aba2b82e5f2049a34eab7806bed183cb252eb
Gonza89 <g.gonzalo10@hotmail.com>
2016-09-04 21:31:11 -0300

    se realiza tp1

---
+++ tp1/js/index.js
@@ -0,0 +1,17 @@
+//ingresar valores
+
+var numero1 = parseInt(prompt("ingrese un número: "));
+var numero2 = parseInt(prompt("ingresar otro número: "));
+
+// operación
+suma = (numero1 + numero2);
+resta = (numero1 - numero2);
+division = (numero1 / numero2);
+multiplicacion = (numero1 * numero2);
+
+// mostrar resultado
+
+alert("la suma es igual a: "+ suma);
+alert("la resta es igual a: "+ resta);
+alert("la division es igual a: "+ division);
+alert("la multiplicacion es igual a: "+ multiplicacion);
