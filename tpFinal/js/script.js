1904f22b5397c1aac5b3d0337917da66505b52a6
Gonza89 <g.gonzalo10@hotmail.com>
2016-10-28 04:02:19 -0300

    agrego tp final

---
+++ tpFinal/js/script.js
@@ -0,0 +1,114 @@
+function Artista (id, nombre, imagen, fav){
+  this.id = id;
+  this.nombre = nombre;
+  this.imagen = imagen;
+  this.fav = fav;
+
+  this.setFav = function (fav) {
+    this.fav = fav;
+  }
+  this.getFav = function () {
+    return this.fav;
+  }
+}
+
+
+
+var Spotify = (function () {
+
+  var artistasFavoritos = [];
+  var claveLocalStorage = 'artistasFavoritos';
+
+  var dibujarArtista = function (artista) {
+    $('<li/>')
+      .attr('id', artista.id)
+      .addClass('list-group-item')
+      .appendTo('#resultadoArtistas');
+
+    var botonFav = $('<button/>')
+      .addClass('btn btn-default')
+      .attr('type', 'button')
+      .on('click', agregarAFavoritos(artista));
+
+    $('<span/>')
+      .addClass('glyphicon glyphicon-star-empty')
+      .html('Favoritos')
+      .appendTo(botonFav);
+    botonFav.appendTo('#' + artista.id);
+
+    $('<h3/>')
+      .html(artista.nombre)
+      .appendTo('#' + artista.id);
+
+    $('<img/>')
+      .attr('src', artista.imagen)
+      .addClass('img-responsive')
+      .css({height: "200px", width: "200px"}).appendTo('#'+ artista.id);
+
+  }
+
+  var buscarArtista = function () {
+    $("#buscarArtistas").on("click", function () {
+      $('#resultadoArtistas').empty();
+      var artista = $("#buscadorArtistas").val();
+      $.ajax({
+        url: "https://api.spotify.com/v1/search?type=artist&q=" + artista,
+        crossDomain: true,
+        dataType: "json"
+      }).done(function (data) {
+        for (obj of data.artists.items){
+          var artista = new Artista(obj.id, obj.name, obj.images[1].url, "");
+          dibujarArtista (artista);
+        }
+      }).fail(function (jqXHR, textStatus) {
+        console.error("textStatus");
+      });
+    });
+  }
+
+  var pestaniaFavoritos = function () {
+    $("#linkFavoritos").on('click', function () {
+      $("#linkFavoritos").parent().addClass("active");
+      $("#linkBuscador").parent().removeClass("active");
+      $("#pestaniaBuscador").removeClass("show").addClass("hidden");
+      $("#pestaniaFavoritos").removeClass("hidden").addClass("show");
+    })
+  }
+
+  var pestaniaBuscador = function () {
+    $("#linkBuscador").on("click", function () {
+      $("#linkBuscador").parent().addClass("active");
+      $("#linkFavoritos").parent().removeClass("active");
+      $("#pestaniaBuscador").removeClass("hidden").addClass("show");
+      $("#pestaniaFavoritos").removeClass("show").addClass("hidden");
+    })
+  }
+
+  agregarAFavoritos = function (artista) {
+    artista.setFav(true);
+    artistasFavoritos.push(artista);
+    guardarArtista();
+    dibujarArtistaEnFavoritos(artista);
+  }
+
+  var guardarArtista = function () {
+    var artistas = JSON.stringify(artistasFavoritos);
+    localStorage.setItem(claveLocalStorage, datos);
+  }
+
+  var iniciar = function () {
+    buscarArtista();
+    pestaniaFavoritos();
+    pestaniaBuscador();
+  }
+  return {
+    iniciar: iniciar
+  };
+})()
+
+
+$(document).ready(function () {
+
+  Spotify.iniciar();
+
+});
